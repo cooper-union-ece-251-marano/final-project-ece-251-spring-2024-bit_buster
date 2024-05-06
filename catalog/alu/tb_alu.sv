134cd19780ea38f6de2f4cad0b8dc437800bbc60
gracetsengg <graet888@icloud.com>
2024-05-06 02:06:34 -0400

    Passed

--- catalog/alu/tb_alu.sv
+++ catalog/alu/tb_alu.sv
@@ -1,11 +1,11 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
-// 
+// Engineers: Lindsey R
+// Engineers: Grace Tseng <grace.tseng@cooper.edu> (made test cases display)
 //     Create Date: 2023-02-07
 //     Module Name: tb_alu
-//     Description: Test bench for simple behavorial ALU
+//     Description: Test bench for simple behavioral ALU
 //
 // Revision: 1.0
 //
@@ -39,48 +39,56 @@ initial begin
     b = 32'h87654321;
     alu_ctrl = 3'b000; 
     #10;
+    $display("Test Add: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test Subtract
     a = 32'h87654321;
     b = 32'h12345678;
     alu_ctrl = 3'b001; 
     #10;
+    $display("Test Subtract: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test AND
     a = 32'habcdef00;
     b = 32'h00ff00ff;
     alu_ctrl = 3'b010; 
     #10;
+    $display("Test AND: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test OR
     a = 32'habcdef00;
     b = 32'h00ff00ff;
     alu_ctrl = 3'b011; 
     #10;
+    $display("Test OR: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test XOR
     a = 32'habcdef00;
     b = 32'h00ff00ff;
     alu_ctrl = 3'b100; 
     #10;
+    $display("Test XOR: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test Shift left
     a = 32'habcdef00;
     b = 5;
     alu_ctrl = 3'b101; 
     #10;
+    $display("Test Shift left: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test Shift right
     a = 32'habcdef00;
     b = 5;
     alu_ctrl = 3'b110; 
     #10;
+    $display("Test Shift right: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // Test Set on less than
     a = 32'h12345678;
     b = 32'habcdef00;
     alu_ctrl = 3'b111; 
     #10;
+    $display("Test Set on less than: a = %h, b = %h, alu_ctrl = %b, result = %h", a, b, alu_ctrl, result);
 
     // End simulation
     #10 $finish;
